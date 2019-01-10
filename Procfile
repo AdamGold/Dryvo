a32a2c5e00b4a50a8546a451b285793cd69f8e18
Adam Goldschmidt <adamgold7@gmail.com>
2019-01-10 11:07:15 +0200

    remove flask session and change procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn  --chdir ./server app:app
+web: gunicorn  --chdir ./server "app:create_app()"
--- server/app_config.py
+++ server/app_config.py
@@ -5,7 +5,6 @@ class Config(object):
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SESSION_TYPE = "filesystem"  # meanwhile, let's use filesystem session
 
     def update(self, newdata):
         for key, value in newdata.items():
