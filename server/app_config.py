f3b63a9e98a932205ab007786a2b3830e5bb57ff
Adam Goldschmidt <adamgold7@gmail.com>
2019-04-17 18:34:29 +0300

    change deployment of database urls to be env specific

--- server/app_config.py
+++ server/app_config.py
@@ -6,7 +6,9 @@ class Config(object):
     SECRET_KEY = os.urandom(24)
     SECRET_JWT = os.environ.get("SECRET_JWT")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        f"{os.environ.get('FLASK_ENV')}_DATABASE_URL".upper()
+    )
     FIREBASE_JSON = os.environ.get("FIREBASE_JSON")
     FACEBOOK_CLIENT_ID = os.environ.get("FACEBOOK_CLIENT_ID")
     FACEBOOK_CLIENT_SECRET = os.environ.get("FACEBOOK_CLIENT_SECRET")
