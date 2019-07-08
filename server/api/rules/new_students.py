765a6b3846a48ccd2fec067d3a5bc69f260e707e
Adam Goldschmidt <adamgold7@gmail.com>
2019-07-08 14:18:18 +0300

    reduce new students rule to hours <=1 score

--- server/api/rules/new_students.py
+++ server/api/rules/new_students.py
@@ -11,7 +11,7 @@ from server.api.rules.utils import register_rule
 
 @register_rule
 class NewStudents(LessonRule):
-    """if a student has less than 5 lessons, blacklist hours <= 3 score"""
+    """if a student has less than 5 lessons, blacklist hours <= 1 score"""
 
     def filter_(self):
         return self.student.lessons_done
@@ -21,6 +21,6 @@ class NewStudents(LessonRule):
             self.date, self.student, self.hours
         ).blacklisted()
         if self.filter_() <= 5 and not more_than_rule["start_hour"]:
-            return {hour.value for hour in self.hours if hour.score <= 3}
+            return {hour.value for hour in self.hours if hour.score <= 1}
 
         return set()
